bc8dc51b68a54f075556013f68ad2505cd568a3e
ZawadiSumba66 <rosesumba98@gmail.com>
2021-06-15 12:41:06 +0300

    Add actions with the APIs

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,48 @@
-export const GET_STOCK = 'GET_STOCK';
-export const CATCH_STOCK_ERROR = 'CATCH_STOCK_ERROR';
-export const LOAD_STOCK = 'LOAD_STOCK';
-export const GET_COMPANY = 'GET_COMPANY';
-export const CATCH_COMPANY_ERROR = 'CATCH_COMPANY_ERROR';
-export const LOAD_COMPANY= 'LOAD_COMPANY';
+import axios from 'axios';
+import {
+  GET_STOCK,
+  LOAD_STOCK,
+  CATCH_STOCK_ERROR,
+  GET_COMPANY,
+  CATCH_COMPANY_ERROR,
+  LOAD_COMPANY,
+} from './type';
+
+const API_KEY = '82e378c6005b855fc6a040e3f63069b6';
+
+const getstock = () => async (dispatch) => {
+  dispatch({
+    type: LOAD_STOCK,
+  });
+  await axios.get(`https://financialmodelingprep.com/api/v3/actives?apikey=${API_KEY}`)
+    .then((response) => {
+      dispatch({
+        type: GET_STOCK,
+        payload: response.data,
+      });
+    }).catch((error) => {
+      dispatch({
+        type: CATCH_STOCK_ERROR,
+        payload: error.message,
+      });
+    });
+};
+
+const getcompany = (ticker) => async (dispatch) => {
+  dispatch({
+    type: LOAD_COMPANY,
+  });
+  await axios.get(`https://financialmodelingprep.com/api/v3/quote/${ticker}?apikey=${API_KEY}`)
+    .then((response) => {
+      dispatch({
+        type: GET_COMPANY,
+        payload: response.data,
+      });
+    }).catch((error) => {
+      dispatch({
+        type: CATCH_COMPANY_ERROR,
+        payload: error,
+      });
+    });
+};
+export { getstock, getcompany };
---
+++ src/actions/type.js
@@ -0,0 +1,6 @@
+export const GET_STOCK = 'GET_STOCK';
+export const CATCH_STOCK_ERROR = 'CATCH_STOCK_ERROR';
+export const LOAD_STOCK = 'LOAD_STOCK';
+export const GET_COMPANY = 'GET_COMPANY';
+export const CATCH_COMPANY_ERROR = 'CATCH_COMPANY_ERROR';
+export const LOAD_COMPANY = 'LOAD_COMPANY';
